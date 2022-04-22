221506a0c1cd7155ee2333cb9465da87b29c2f70
Jerwos <mejia.fonseca2009@gmail.com>
2022-04-22 06:02:49 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles.scss';
+function myFunction() {
+  var x = document.getElementById('menu');
+  if (x.className === 'menu') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'menu';
+  }
+}
